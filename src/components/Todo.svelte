2902ca2525636ae71c3ac4a5d073cdc38b245609
aryawirasandi <aryawirasandi7153@gmail.com>
2023-02-04 21:51:39 +0800

    add id testing for Todo component

--- src/components/Todo.svelte
+++ src/components/Todo.svelte
@@ -5,7 +5,7 @@
     export let userId;
 </script>
 
-<div class="my-5 p-10 bg-white shadow-lg border-solid border-black w-100 relative">
+<div class="my-5 p-10 bg-white shadow-lg border-solid border-black w-100 relative"  data-testid="todolists">
     <div class="bg-red-500 h-10 w-10 absolute flex items-center justify-center left-0 top-0 rounded-br-xl">
         <p class="text-white">{id}</p>
     </div>
@@ -14,11 +14,11 @@
         <p class="text-base text-small">Created by user {userId}</p>
     </div>
     <hr class="my-5">
-    <div>
+    <div id="status">
         <p>
             <span class="mr-5"> Status: </span>
             {#if completed}
-                <strike class="text-white bg-green-500 p-3">Finish</strike>
+                <span class="text-white bg-green-500 p-3">Finish</span>
             {:else}
                 <span class="text-white bg-red-500 p-3">Not finish</span>
             {/if}
