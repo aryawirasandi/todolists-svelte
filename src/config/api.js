f871bafc18ba54ab3858e537cdd5579ba21baf14
aryawirasandi <aryawirasandi7153@gmail.com>
2023-02-06 21:07:00 +0800

    add api helper for api call

--- src/config/api.js
+++ src/config/api.js
@@ -1,9 +1,24 @@
 
-export async function apiWrapper(query) {
-    try{
-        const response = await fetch(`${import.meta.env.VITE_BASE_URL}/${query}`);
+export async function apiWrapper(query, option) {
+    try {
+        const response = await fetch(`${import.meta.env.VITE_BASE_URL}/${query}`, option);
         return response.json();
-    }catch(error){
+    } catch (error) {
+        throw new Error(`Error : ${error}`)
+    }
+}
+
+export async function store(query, { method = "POST", body }) {
+    try {
+        let response = await apiWrapper(query, {
+            method,
+            body: JSON.stringify(body),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8'
+            }
+        });
+        return response;
+    } catch (error) {
         throw new Error(`Error : ${error}`)
     }
 }
