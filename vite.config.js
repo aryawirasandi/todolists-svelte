aaa72c1daf9daa61842b0236bfe4d8decd4fb423
aryawirasandi <aryawirasandi7153@gmail.com>
2023-02-09 12:31:30 +0800

    add dependency support for fetch in node

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTests.js'],
+    deps: {
+      inline: [
+        "whatwg-fetch"
+      ]
+    }
+  }
 })
