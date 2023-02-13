c9ba0b564f8c2ca712b8231f4385e5ea4710f672
aryawirasandi <aryawirasandi7153@gmail.com>
2023-02-13 14:10:27 +0800

    add unit tests for delete todos and set another to todo testing or in progress testing

--- src/test/app.test.js
+++ src/test/app.test.js
@@ -1,10 +1,10 @@
-import { render, cleanup, fireEvent, screen, waitFor } from "@testing-library/svelte";
+import { render, cleanup, fireEvent, screen, waitFor, waitForElementToBeRemoved } from "@testing-library/svelte";
 import App from "../App.svelte";
 
 describe("App.svelte", () => {
     let el;
-    beforeEach(() => {
-        el = render(App);
+    beforeEach(async () => {
+        el = await render(App);
     })
     afterEach(() => {
         cleanup();
@@ -16,29 +16,38 @@ describe("App.svelte", () => {
 
     it("get todo lists", async () => {
         expect(screen.getByText("Loading....")).toBeTruthy();
-        await waitFor(() => screen.getAllByTestId("todolists"));
-        expect(screen.queryAllByTestId('todolists')[0]).toBeTruthy()
-        expect(screen.queryAllByTestId('todolists')[1]).toBeTruthy()
-        expect(screen.queryAllByTestId('todolists')[2]).toBeFalsy();
-        expect(screen.queryAllByTestId('todolists')[0].querySelector("#status > p > span:nth-child(2)").textContent).toEqual("Not finish");
-        expect(screen.queryAllByTestId('todolists')[1].querySelector("#status > p > span:nth-child(2)").textContent).toEqual("Finish");
+        expect(await screen.findByTestId('todo-1')).toBeTruthy()
+        expect(await screen.findByTestId('todo-2')).toBeTruthy()
+        expect(screen.queryByTestId('todo-3')).toBeFalsy();
+        expect(await (await screen.findByTestId('todo-1')).querySelector("#status > p > span:nth-child(2)").textContent).toEqual("Not finish");
+        expect(await (await screen.findByTestId('todo-2')).querySelector("#status > p > span:nth-child(2)").textContent).toEqual("Finish");
     });
 
-    it.todo("submit todo at form", async () => {
 
-        const buttonAdd = el.getByTestId("add-todo");
+    it("can delete a todo", async () => {
+        const targetId = 1;
+        const todoDeleteTrigger = await screen.findByTestId(`todo-delete-${targetId}`);
+        const selectedTodo = await screen.findByTestId(`todo-${targetId}`);
+        expect(selectedTodo).toBeTruthy();
+        await fireEvent.click(todoDeleteTrigger);
+        expect(selectedTodo).not.toBeTruthy();
+    })
+
+    it("submit todo at form", async () => {
+
+        const buttonAdd = screen.getByTestId("add-todo");
 
         await fireEvent.click(buttonAdd)
 
-        const form = el.getByTestId("form-submit");
+        const form = screen.getByTestId("form-submit");
 
         expect(form).toBeTruthy();
 
         const input = form.querySelector("input[type='text']");
-        const userSelect = el.getByTestId("userSelect");
-        const statusSelect = el.getByTestId("statusSelect");
-        const userSelectOption = el.getAllByTestId("user-option");
-        const submitData = el.getByTestId("submit-data");
+        const userSelect = screen.getByTestId("userSelect");
+        const statusSelect = screen.getByTestId("statusSelect");
+        const userSelectOption = screen.getAllByTestId("user-option");
+        const submitData = screen.getByTestId("submit-data");
 
 
         fireEvent.change(userSelect, {
@@ -61,29 +70,93 @@ describe("App.svelte", () => {
             }
         });
 
-        await fireEvent.click(submitData);
+        expect(input.value).toEqual("A Testing Todo Here");
+        expect(statusSelect.value).toBe('true');
 
-        await waitFor(() => screen.getAllByTestId("modal-success"))
+        fireEvent.click(submitData);
 
-        expect(form).not.toBeTruthy();
 
-        expect(screen.getByTestId("modal-success")).toBeTruthy();
-
-        const element = el.queryAllByTestId("todolists");
-
-        expect(element[0]).toBeTruthy();
-
-        expect(modal).toBeInTheDocument();
-
+        // await waitFor(() => expect(screen.getByText("A Testing Todo Here")).toBeTruthy()); // bug unit testing is not sync with svelte data
+        // expect(el.getByText("A Testing Todo Here")).toEqual("A Testing Todo Here")
     })
 
-    it.todo("can update a todo", () => {
+    it.todo("can update a todo", async () => {
+        const targetId = 1;
+        const todoUpdateTrigger = el.getByTestId(`update-todo-${targetId}`);
+        const expectedText = "sunt aut facere repellat provident occaecati excepturi optio reprehenderit";
+        const postText = "A Testing Todo Here";
+
+        await fireEvent.click(todoUpdateTrigger);
+
+        const form = el.getByTestId("form-submit");
+
+        expect(form).toBeTruthy();
+
+        const updateInput = form.querySelector("input[type='text']");
+        const userSelect = el.getByTestId("userSelect");
+        const statusSelect = el.getByTestId("statusSelect");
+        const userSelectOption = el.getAllByTestId("user-option");
+        const updateData = el.getByTestId("update-data");
+
+        expect(updateInput.value).toBe(expectedText);
+        expect(userSelect.value).toBe(1);
+        expect(statusSelect.value).toBe(false);
+        expect(userSelectOption.value).toBe(1);
+
+        fireEvent.change(updateInput, {
+            target: {
+                value: postText
+            }
+        });
+
+        fireEvent.change(userSelect, {
+            target: {
+                value: 2
+            }
+        });
+
+        fireEvent.change(statusSelect, {
+            target: {
+                value: true,
+            }
+        });
+
+        fireEvent.change(userSelectOption, {
+            target: {
+                value: 2
+            }
+        });
+
+        await fireEvent.click(updateData);
+
+
+        expect(updateInput.value.length).toBe(0);
+        expect(userSelect.value).toBe(1);
+        expect(statusSelect.value).toBe(false);
+        expect(userSelectOption.value).toBe(1);
+
+        const selectedTodo = el.getByTestId(`todo-${targetId}`);
+
+        expect(selectedTodo.getByText("A Testing Todo Here")).toBeTruthy();
+        expect(selectedTodo.getByText(`Created by user ${targetId}`)).toBeTruthy();
+        expect(selectedTodo.getByText("Finish")).toBeTruthy();
 
     });
 
-    it.todo("can show a todo", () => { })
+    it.todo("can show a todo", async () => {
+        const targetId = 1;
+        const todoUpdateTrigger = el.getByTestId(`todo-show-${targetId}`);
+        const expectedText = "sunt aut facere repellat provident occaecati excepturi optio reprehenderit";
 
-    it.todo("can delete a todo", () => { })
+        await fireEvent.click(todoUpdateTrigger);
+
+        const modalDisplay = screen.getByTestId("modal-display-todo");
+
+        expect(modalDisplay).toBeTruthy();
+
+        expect(modalDisplay.querySelector("p:nth-child(1)").textContent).toBe(`Created by user ${1}`)
+        expect(modalDisplay.querySelector("p:nth-child(2)").textContent).toBe(expectedText);
+    })
 
     it("select the option user", async () => {
         const button = el.getByTestId("add-todo");
@@ -141,6 +214,4 @@ describe("App.svelte", () => {
         expect(statusSelectOption[1].selected).toBeTruthy();
         expect(statusSelectOption[0].selected).toBeFalsy();
     });
-
-    it.todo("get todo lists with request api", () => { });
 })
