54faf5c14b6a7850ea22fb660e867de76ba21427
aryawirasandi <aryawirasandi7153@gmail.com>
2023-02-14 18:47:21 +0800

    add unit testing mocking  request api

---
+++ src/setupTests.js
@@ -0,0 +1,7 @@
+import { server } from "./mocks/server";
+import "whatwg-fetch"
+beforeAll(() => server.listen());
+
+afterEach(() => server.resetHandlers());
+
+afterAll(() => close());
--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 export default defineConfig({
   plugins: [svelte()],
   test: {
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     globals: true,
     environment: 'jsdom',
     setupFiles: ['./src/setupTests.js'],
