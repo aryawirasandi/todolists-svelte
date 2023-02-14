aab69196ebeb65cd4a951fbb586f4080d3f77996
aryawirasandi <aryawirasandi7153@gmail.com>
2023-02-14 18:48:30 +0800

    add event dispatcher for delete todo

--- src/components/Todo.svelte
+++ src/components/Todo.svelte
@@ -1,14 +1,20 @@
 <script>
+    import { createEventDispatcher } from "svelte"
     export let completed;
     export let id;
     export let title;
     export let userId;
+    
+    const dipatch = createEventDispatcher();
 </script>
 
-<div class="my-5 p-10 bg-white shadow-lg border-solid border-black w-100 relative"  data-testid="todolists">
+<div class="my-10 p-10 bg-white shadow-lg border-solid border-black w-100 relative"  data-testid="todo-{id}" >
     <div class="bg-red-500 h-10 w-10 absolute flex items-center justify-center left-0 top-0 rounded-br-xl">
         <p class="text-white">{id}</p>
     </div>
+    <button on:click="{() => dipatch('delete', id)}" class="bg-red-500 h-10 w-10 absolute flex items-center justify-center w-36 cursor-pointer right-0 top-0 rounded-bl-xl text-white" data-testid="todo-delete-{id}">
+        delete
+    </button>
     <div>
         <p class="text-4xl text-gray">{title}</p>
         <p class="text-base text-small">Created by user {userId}</p>
