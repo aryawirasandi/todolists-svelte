7f81147c21b7a1b631fec495a5151108039401ab
aryawirasandi <aryawirasandi7153@gmail.com>
2023-02-14 18:46:46 +0800

    update several tests id and some animation

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,20 @@
 <script>
     import { onMount } from "svelte";
+    import { fly, fade } from "svelte/transition"
+    import { cubicOut } from "svelte/easing";
     import { apiWrapper, store } from "./config/api";
     import Todo from "./components/Todo.svelte";
     export let todos = [];
     let users = [1,2,3,4,5]
     let selected = users[0];
-    let condition = false;
+    let isCompleted = false;
     let toggleForm = false;
-    let todo = "";
     let toggleModal = false;
-    
+    let todo = "";    
     async function getTodos(){
         try {
             let response = await apiWrapper("todos");
-            todos = response;
+            todos = response.slice(0, 10);
         } catch (error) {
             throw new Error("Someting wrong : " + error);
         }
@@ -21,33 +22,38 @@
 
     let todosRequest = getTodos();
 
-    async function createTodos(){
+    async function createTodo(){
         try {
             let response = await store("todos", {
                 body : {
-                    title : todo,
-                    body: todo,
-                    completed: condition,
-                    user: selected
+                    id: Math.floor(Math.random() * 100),
+                    title: todo,
+                    completed: isCompleted,
+                    userId: selected
                 }
             });
-            console.log(response);
+            toggleForm = !toggleForm;
+            toggleModal = !toggleModal;
+            setTimeout(() => {
+                toggleModal = !toggleModal;
+            }, 1000)
+            todos = [...todos, response];
         } catch (error) {
             throw new Error("Something wrong : " + error);
         }
     }
 
 
+    function removeTodo(event){
+        
+        const { detail } = event;
+        const todosFiltered = todos.filter(todo => todo.id != detail);
+        todos = todosFiltered;
+    }
+    
     function triggerModal(){
         toggleForm = !toggleForm;
     }
-
-    function storeTodo(){
-        // toggleForm = !toggleForm;
-        // toggleModal = !toggleModal;
-        createTodos();
-    }
-    
 </script>
 
 <main class="relative p-4">
@@ -55,11 +61,13 @@
         <form 
             data-testid="form-submit"
             class="z-1 border-solid border-black border-2 flex flex-col w-1/4 p-10 absolute right-1/2 top-[1/2] mt-10 translate-x-1/2 bg-blue-200"
-            on:submit|preventDefault={storeTodo}
+            on:submit|preventDefault={createTodo}
+            in:fly="{{ delay : 500}}"
+            out:fade
             >
             <label for="user">
                 User Selected
-                <select data-testid="userSelect" bind:value={selected} class="w-full">
+                <select data-testid="userSelect" bind:value={selected} class="w-full p-3 border-solid border-blue-500 border-2">
                     <option disabled>Select your user</option>
                     {#each users as user }
                         <option data-testid="user-option" value={user}>{user}</option>
@@ -71,11 +79,14 @@
                     Title
                     <input type="text"  data-testid="title" placeholder="input your todo" name="title" class="p-3 border-solid border-blue-500 border-2 focus:outline-double w-full" bind:value={todo}>
                 </label>
+                <p data-testid="result">
+                    {todo}
+                </p>
             </div>
             <div class="my-2">
                 <label for="status">
                     Status
-                    <select data-testid="statusSelect" name="status" bind:value={condition} class="p-3 border-solid border-blue-500 border-2 focus:outline-double w-full">
+                    <select data-testid="statusSelect" name="status" bind:value={isCompleted} class="p-3 border-solid border-blue-500 border-2 focus:outline-double w-full">
                         <option disabled selected>Select status of todo </option>
                         <option data-testid="status-option" value={true}>Finish</option>
                         <option data-testid="status-option" value={false}>Not Finish Finish</option>
@@ -83,12 +94,12 @@
                 </label>
             </div>
             <div class="my-2">
-                <button class="border-white border-solid bg-gray-400 text-white p-3 rounded-lg w-full" data-testid="submit-data">Submit</button>
+                <button class="border-white border-solid bg-blue-500 opacity-50 hover:opacity-100 transition ease-in delay-200 text-white p-3 rounded-lg w-full" data-testid="submit-data">Submit</button>
             </div>
         </form>
     {/if}
     {#if toggleModal}
-        <div class="border-solid border-black border-2 flex flex-col w-1/4 p-10 absolute z-1 right-1/2 top-[1/2] mt-10 translate-x-1/2 bg-blue-200" data-testid="modal-success">
+        <div data-testid="modal-success" class="border-solid border-black border-2 flex flex-col w-1/4 p-10 absolute z-1 right-1/2 top-[1/2] mt-10 translate-x-1/2 bg-blue-200">
             <div class="text-center">
                 <p>Success Add Todo</p>
             </div>
@@ -110,7 +121,7 @@
                 </div>
             {:else}
                 {#each todos as todo}
-                    <Todo {...todo}/>
+                    <Todo {...todo} on:delete={removeTodo}/>
                 {/each}
             {/if}
         {:catch error}
@@ -119,6 +130,6 @@
     </div>
 </main>
 {#if toggleForm}
-    <button class="bg-black top-0 opacity-25 absolute w-full h-full" on:click={triggerModal}>
+    <button in:fly out:fade class="bg-black top-0 opacity-25 absolute w-full h-full" on:click={triggerModal}>
     </button>  
 {/if}
